e0a87ed17f09174fb97afe4e3bdf6479301b995f
davidjm macbook <djmlewis@mac.com>
2023-03-27 08:51:38 +0100

    Initial Commit

---
+++ colours/coloursApp.swift
@@ -0,0 +1,17 @@
+//
+//  coloursApp.swift
+//  colours
+//
+//  Created by David JM Lewis on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct coloursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
